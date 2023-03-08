f521196ba7f7f9acdc28fb095a68053daed2a513
Macbook <macbook@morella.macopedia>
2023-03-08 14:02:08 +0100

    [FRONT-02] added components, splitted css

---
+++ components/FurnitureTile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="furniture-types__tile">
+        <slot></slot>
+    </div>
+</template>
+<style scoped>
+.furniture-types__tile{
+    width: 45%;
+    margin: 10px auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 35px;
+    padding: 70px 0;
+    border-radius: 40px;
+    background-color: #AED9ED;
+    color: #fff;
+}
+.furniture-types__tile:hover{
+    background-color: #8ecae6;
+    cursor: pointer;
+}
+.furniture-types__tile *{
+    margin: 0 10px;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,11 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: {
+    dirs: [
+      '~/components'
+    ]
+  },
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
