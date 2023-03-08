c44a91739b1d90c2a661fda090039dc68075b3d4
Macbook <macbook@morella.macopedia>
2023-03-08 15:52:47 +0100

    [FRONT-00] adding styles, editing templates

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,16 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: "https://cdn.jsdelivr.net/gh/iconoir-icons/iconoir@main/css/iconoir.css" }
+    ],
+    bodyAttrs: {
+      class: 'body'
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
+  css: ["~/assets/css/main.css"
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
